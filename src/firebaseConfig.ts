97ffac6762447691bac4b7d1d7ba55800e0d872e
Rodrigo Borges <rodrigo-b1@outlook.com>
2025-06-20 19:03:30 -0300

    Sistema de ranking

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,16 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKV6g6m_6vA44pMFKz1GhAi2i2Rn_FxdI",
-  authDomain: "farflag-fe337.firebaseapp.com",
-  projectId: "farflag-fe337",
-  storageBucket: "farflag-fe337.firebasestorage.app",
-  messagingSenderId: "1006054586756",
-  appId: "1:1006054586756:web:9216dd12e6636251ce05d1",
-  measurementId: "G-W4HHQNWQK3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 export { db };
